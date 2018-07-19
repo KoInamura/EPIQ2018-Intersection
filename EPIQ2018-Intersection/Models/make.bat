2bc1b90f5d440e3816a7aed87f6be196b308730a
koinamura <koinamura@10.81.11.228>
2018-07-19 17:56:14 -0500

    EPIQ2018-Intersection - Initializing

---
+++ EPIQ2018-Intersection/Models/make.bat
@@ -0,0 +1,2 @@
+@echo off
+call quorum sourcecode\main.quorum sourcecode\crab.quorum sourcecode\wall.quorum sourcecode\targetko.quorum sourcecode\pokemongoer.quorum sourcecode\rock.quorum
--- EPIQ2018-Intersection/make.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call quorum sourcecode\main.quorum sourcecode\crab.quorum sourcecode\wall.quorum sourcecode\targetko.quorum sourcecode\pokemongoer.quorum sourcecode\rock.quorum
